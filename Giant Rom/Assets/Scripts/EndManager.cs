17cfea66b0372a510eb8303d720a7ebf6be5193c
Timothy ONeill <toneill22@qub.ac.uk>
2016-08-13 20:17:37 +0100

    Fail state for spectators

--- Giant Rom/Assets/Scripts/EndManager.cs
+++ Giant Rom/Assets/Scripts/EndManager.cs
@@ -15,7 +15,7 @@ public class EndManager : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+    }
 }
--- Giant Rom/Assets/Scripts/Spectator.cs
+++ Giant Rom/Assets/Scripts/Spectator.cs
@@ -9,8 +9,10 @@ public class Spectator : MonoBehaviour {
     enum Mood { bored, likes, hates, silly };
     public StatBar statBar;
     public SpriteRenderer emotionRenderer;
+    public SpriteRenderer failureRenderer;
     public Sprite[] emotions;
     bool fullReset = false;
+    bool playerFailed = false;
 
     // Use this for initialization
     void Start ()
@@ -31,38 +33,53 @@ public class Spectator : MonoBehaviour {
                 fullReset = false;
             }
         }
+
+        if (!playerFailed && statBar.GetFillamount() == 0.0f)
+        {
+            statBar.gameObject.SetActive(false);
+            emotionRenderer.sprite = null;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 50);
+            failureRenderer.enabled = true;
+            playerFailed = true;
+        }
     }
 
     public void Feelings(int _demonstration)
     {
+        if (statBar.GetFillamount() != 0.0f)
+        {
+            if ((int)Mood.likes == _demonstration)
+            {
+                statBar.ChangeBar(0.0005f);
+                emotionRenderer.sprite = emotions[0];
+            }
 
-        if((int)Mood.likes == _demonstration)
-        {
-            statBar.ChangeBar(0.0005f);
-            emotionRenderer.sprite = emotions[0];
-        }
+            else if ((int)Mood.hates == _demonstration)
+            {
+                statBar.ChangeBar(-0.0005f);
+                emotionRenderer.sprite = emotions[1];
+            }
+            else
+            {
+                statBar.ChangeBar(0);
+                emotionRenderer.sprite = emotions[2];
+            }
 
-        else if((int)Mood.hates == _demonstration)
-        {
-            statBar.ChangeBar(-0.0005f);
-            emotionRenderer.sprite = emotions[1];
-        }
-        else
-        {
-            statBar.ChangeBar(0);
-            emotionRenderer.sprite = emotions[2];
-        }
-
-        if ((int)Mood.silly == _demonstration) //Silliness.
-        {
-            fullReset = true;
-            //statBar.ChangeBar(0.05f);
-            emotionRenderer.sprite = emotions[0];
+            if ((int)Mood.silly == _demonstration) //Silliness.
+            {
+                fullReset = true;
+                emotionRenderer.sprite = emotions[0];
+            }
         }
     }
 
     public void CantSee()
     {
         statBar.TempChangeBar(-0.0005f);
+    }
+
+    public bool GetPlayerFailed()
+    {
+        return playerFailed;
     }
 }
