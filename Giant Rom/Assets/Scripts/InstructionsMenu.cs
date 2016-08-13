3c643e098dec9e0819e6f28dbdfcba56344baaba
Timothy ONeill <toneill22@qub.ac.uk>
2016-08-13 15:29:28 +0100

    StartMenu, Instructions Menu

---
+++ Giant Rom/Assets/Scripts/InstructionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class InstructionsMenu : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("DanTest");
+        }
+    }
+}
