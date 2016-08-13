8b4fdd0ff2bec1cdbde58e36ed83d2c03cacd272
Cwaff <ccraig22@qub.ac.uk>
2016-08-13 15:29:59 +0100

    End Scene

---
+++ Giant Rom/Assets/EndManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndManager : MonoBehaviour {
+
+
+   public AudioClip[] audio;
+    public AudioSource source;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        source.PlayOneShot(audio[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   
+}
